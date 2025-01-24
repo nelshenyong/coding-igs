4fdd7ae330e761a2e9a14f1e8f2df67250dbe30b
nelshenyong <nelshenyong@gmail.com>
2025-01-24 09:09:42 +0700

    update

---
+++ IGS Procode/FLASK_IGS11_S2/MEET03/main.py
@@ -0,0 +1,24 @@
+from flask import *
+
+app = Flask('__name__')
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/bio', methods=['GET', 'POST'])
+def bio():
+    render_template('bio.html',
+                    nama = request.form['nama'],
+                    kelas = request.form['kelas'],
+                    email = request.form['email'],
+                    tempat = request.form['tempat'],
+                    tgl = request.form['tgl'],
+                    cita = request.form['cita'],
+                    agama = request.form['agama'],
+                    tentang = request.form['tentang']
+                    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
