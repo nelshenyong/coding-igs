0d4fb1b2ab1111f154c6eff759798da8c25b1e25
nelshenyong <nelshenyong@gmail.com>
2025-01-24 09:23:53 +0700

    update

--- IGS Procode/FLASK_IGS11_S2/MEET03/main.py
+++ IGS Procode/FLASK_IGS11_S2/MEET03/main.py
@@ -9,16 +9,17 @@ def home():
 
 @app.route('/bio', methods=['GET', 'POST'])
 def bio():
-    render_template('bio.html',
-                    nama = request.form['nama'],
-                    kelas = request.form['kelas'],
-                    email = request.form['email'],
-                    tempat = request.form['tempat'],
-                    tgl = request.form['tgl'],
-                    cita = request.form['cita'],
-                    agama = request.form['agama'],
-                    tentang = request.form['tentang']
-                    )
+    return render_template('bio.html',
+                           nama = request.form['nama'],
+                           kelas = request.form['kelas'],
+                           email = request.form['email'],
+                           tempat = request.form['tempat'],
+                           tgl = request.form['tgl'],
+                           cita = request.form['cita'],
+                           agama = request.form['agama'],
+                           tentang = request.form['tentang']
+                           )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
